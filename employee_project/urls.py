d6cd0015b8f46d74265ee03c95c4b93df3774d13
meghnathachil <meghnathachil@gmail.com>
2025-04-30 18:25:49 -0400

    Update urls.py

--- employee_project/urls.py
+++ employee_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
-from django.urls import path, include  # ✅ add include here
+from django.urls import path, include  
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
